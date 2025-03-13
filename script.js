01acc0e9f3b09502bcb18dba52d44d316cc86847
Mr-Nikhil-jha <nikhil324008@jha@gmail.com>
2025-03-13 14:32:55 +0530

    weather

---
+++ script.js
@@ -0,0 +1,109 @@
+//api key variable
+let APIkey;
+APIkey = "aa3e61c5be74d68e22ae861cef48d33d";
+const wrapper = document.querySelector(".wrapper"),
+    inputPart = document.querySelector(".input-part"),
+    infoTxt = inputPart.querySelector(".info-txt"),
+    inputField = inputPart.querySelector("input"),
+    locationBtn = inputPart.querySelector("button"),
+    // weatherpart = wrapper.querySelector(".weather-part"),
+    wIcon = wrapper.querySelector(" .weather-part img"),
+    arrowBack = wrapper.querySelector("header i");
+
+let api;
+
+inputField.addEventListener("keyup", (e) => {
+    if (e.key == "Enter" && inputField.value != "") {
+        requestApi(inputField.value);
+    }
+});
+locationBtn.addEventListener("click", () => {
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(onSuccess, onError);
+    } else {
+        alert("Your browser not support geolocation api");
+    }
+});
+
+function onSuccess(position) {
+    const { latitude, longitude } = position.coords;
+    api = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&units=metric&appid=${APIkey}`;
+    fetchData();
+}
+
+function onError(error) {
+    infoTxt.innerText = error.message;
+    infoTxt.classList.add("error");
+}
+
+function requestApi(city) {
+    api = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${APIkey}`;
+    fetchData();
+}
+
+function fetchData() {
+    infoTxt.innerText = "Getting weather details...";
+    infoTxt.classList.add("pending");
+    //getting api response and returning it with parsing into js obj and in another
+    //then function callin weatherDetails function with passing api result as an argument
+    fetch(api)
+        .then((response) => response.json())
+        .then((result) => weatherDetails(result));
+}
+
+function weatherDetails(info) {
+    infoTxt.classList.replace("pending", "error");
+    if (info.cod == "404") {
+        infoTxt.innerText = `${inputField.value} isn't a valid city name`;
+    } else {
+        // let's get required properties value from the info object
+        const city = info.name;
+        const country = info.sys.country;
+        const { description, id } = info.weather[0];
+        const { feels_like, humidity, temp } = info.main;
+
+        //using custom icon according to the id which api return us
+        if (id == 800) {
+            wIcon.src = "icons/clear.svg";
+        } else if (id >= 200 && id <= 232) {
+            wIcon.src = "icons/storm.svg";
+        } else if (id >= 600 && id <= 622) {
+            wIcon.src = "icons/snow.svg";
+        } else if (id >= 701 && id <= 781) {
+            wIcon.src = "icons/haze.svg";
+        } else if (id >= 801 && id <= 804) {
+            wIcon.src = "icons/cloud.svg";
+        } else if ((id >= 500 && id <= 531) || (id >= 300 && id <= 321)) {
+            wIcon.src = "icons/rain.svg";
+        }
+
+        // lets pass these values to a particular html element
+        wrapper.querySelector(".temp .numb").innerText = Math.floor(temp);
+        wrapper.querySelector(".weather").innerText = description;
+        wrapper.querySelector(".locations span").innerText = `${city}, ${country}`;
+        wrapper.querySelector(".temp .numb-2").innerText = Math.floor(feels_like);
+        wrapper.querySelector(".humidity span").innerText = `${humidity}%`;
+        inputField.value = "";
+        infoTxt.classList.remove("pending", "error");
+        wrapper.classList.add("active");
+    }
+}
+
+arrowBack.addEventListener("click", () => {
+    wrapper.classList.remove("active");
+});
+
+// const disabledKeys = ["c", "C", "x", "u", "I"]; // keys that will be disabled
+//       const showAlert = e => {
+//         e.preventDefault(); // preventing its default behaviour
+//         return alert("Sorry, you can't view or copy source codes this way!");
+//       }
+//       document.addEventListener("contextmenu", e => {
+//         showAlert(e); // calling showAlert() function on mouse right click
+//       });
+//       document.addEventListener("keydown", e => {
+//         // calling showAlert() function, if the pressed key matched to disabled keys
+//         if(e.ctrlKey && disabledKeys.includes(e.key) || e.key === "F12") {
+//           showAlert(e);
+//         }
+//       });
